29d1ca868dc5401f10cbe59112b5c660372cccf1
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-07-26 22:06:48 +0530

    Time: 0 ms (100.00%), Space: 13 MB (49.81%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,22 +1,17 @@
-#define op ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL) ;
+#define op ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         op;
         int n = prices.size();
-        vector<int> forward(2,0), curr(2,0);
-        forward[0] = forward[1] = 0 ;
+        int forwardBuy,forwardNotbuy,currentBuy,currentNotbuy;
+        forwardNotbuy = forwardBuy = 0 ;
         for(int i=n-1;i>=0; --i) {
-            for(int j=0; j<=1; ++j) {
-                int profit = 0 ;
-                if(j) 
-                    profit = max(-prices[i]+forward[0],forward[1]);
-                else 
-                    profit = max(prices[i]+forward[1],forward[0]);
-                curr[j] = profit;
-            }
-            forward = curr;
+			currentBuy = max(-prices[i]+forwardNotbuy,forwardBuy);
+			currentNotbuy = max(prices[i]+forwardBuy,forwardNotbuy);
+            forwardBuy = currentBuy;
+			forwardNotbuy = currentNotbuy;
         }
-        return forward[1];
+        return forwardBuy;
     }
 };
