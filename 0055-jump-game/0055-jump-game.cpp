858871ef275d99c38540157ef2b37dc6547e72e7
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-07-28 16:07:08 +0530

    Time: 2230 ms (5.01%), Space: 51.1 MB (10.60%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,19 +1,15 @@
-#define op ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 class Solution {
 public:
     bool solve(int ind, int n, vector<int>& arr,vector<int>& dp){
-        op;
         if(ind >= n) return true;
         if(dp[ind] != -1) return dp[ind];
         bool ans = false;
-        for(int i=1; i<=arr[ind]; i++) {
-            ans = solve(ind+i,n,arr,dp);
-            if(ans) break;
+        for(int i=ind+1; i<=min(n,ind+arr[ind]); i++) {
+            ans = ans || solve(i,n,arr,dp);
         }
         return dp[ind] = ans;
     }
     bool canJump(vector<int>& nums) {
-        op;
         vector<int> dp(nums.size(),-1);
         return solve(0,nums.size()-1,nums,dp);
     }
