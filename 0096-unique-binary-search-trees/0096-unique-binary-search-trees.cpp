b44785b94c4bf7d8492a3504af8781352092405d
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-08-05 10:23:52 +0530

    Time: 2 ms (49.17%), Space: 6.1 MB (10.18%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,19 +1,15 @@
 #define op ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution {
 public:
-    int solve(int n, vector<int>& dp)
-    {
-        op;op;
-        if(n <= 1) return 1;
-        if(dp[n]) return dp[n];
-        for(int i = 1; i <= n; i++) 
-            dp[n] += solve(i-1,dp) * solve(n-i,dp);
-        return dp[n];
-    }
-
     int numTrees(int n) {
         op;op;
         vector<int> dp(n+1,0);
-        return solve(n,dp);
+        dp[0] = dp[1] = 1;
+        for(int i=2; i<=n; ++i) {
+            for(int j=1; j<=i; ++j) {
+                dp[i] += dp[j-1] * dp[i-j]; 
+            }
+        }
+        return dp[n];
     }
 };
