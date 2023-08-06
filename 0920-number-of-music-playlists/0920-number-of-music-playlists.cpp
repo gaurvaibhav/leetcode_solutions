7d77c429a2886a2fb0c1e9aaabd4b74485fd7eaf
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-08-06 19:11:29 +0530

    Time: 10 ms (23.87%), Space: 12.8 MB (5.86%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -0,0 +1,22 @@
+#define op ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+typedef long long ll;
+class Solution {
+public:
+    ll MOD = 1e9+7;
+    ll solve(int ind, int g, int k, int n, vector<vector<ll>>& dp){ 
+        op;op;
+        if(!g) return (ind == n);
+        if(dp[ind][g]!=-1) return dp[ind][g];
+        ll res = 0;
+        res = (res+((n-ind)*solve(ind+1,g-1,k,n,dp))%MOD)%MOD;
+        if(ind >= k) 
+            res = (res+((ind-k)*solve(ind,g-1,k,n,dp))%MOD)%MOD;
+        return dp[ind][g] = res%MOD;
+    }
+    int numMusicPlaylists(int n, int goal, int k) {
+        op;op;
+        vector<vector<ll>> dp(n+goal,vector<ll> (goal+1,-1));
+        return solve(0,goal,k,n,dp);
+    }
+};
+
