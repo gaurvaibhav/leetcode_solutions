75fa0b4b0485b6fedffcd39f9ea2fafe2249d578
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-08-22 23:00:33 +0530

    Time: 7 ms (72.92%), Space: 13.2 MB (33.79%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -2,21 +2,18 @@
 #define MOD 1000000007
 #define ll long long
 class Solution {
-private:
-    int solve(int n, vector<ll>& dp) {
-        op; 
-        if(n<=0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int take, notTake;
-        take  = 1+solve(n-2, dp);
-        notTake = solve(n-1, dp);
-        return dp[n] = (take + notTake)%MOD;
-    }
 public:
     int countHousePlacements(int n) {
         op;
-        vector<ll> dp(n+1,-1);
-        ll res = solve(n, dp);
+        vector<ll> dp(n+1,0);
+        dp[1]=1;
+        for(int i=2; i<=n; ++i) {
+            int take, notTake;
+            take  = 1+dp[i-2];
+            notTake = dp[i-1];
+            dp[i] = (take + notTake)%MOD;
+        }
+        ll res = dp[n];
         res = ((2*res)+(res*res))%MOD;
         return (res+1)%MOD;
     }
