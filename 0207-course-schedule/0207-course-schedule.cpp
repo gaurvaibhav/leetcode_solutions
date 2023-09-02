9adcfbebd6b3af15e3da6a1c5d993a7cfbf6ceed
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-02 10:36:39 +0530

    Time: 15 ms (94.18%), Space: 14 MB (41.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,33 @@
 #define op ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution
 {
+private:
+    bool dfsChk(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& pathvis)
+    {
+        op;
+        vis[node] = 1;
+        pathvis[node] = 1;
+        for(auto& it: adj[node]) {
+            if(!vis[it]) {
+                if(dfsChk(it, adj, vis, pathvis)) return  true;
+            }
+            else if(pathvis[it]) return true;
+        }
+        pathvis[node] = 0;
+        return false;
+    }
+    bool isCyclic(vector<vector<int>>& adj, int v)
+    {
+        op;
+        vector<int> vis(v, 0);
+        vector<int> pathvis(v, 0);
+        for(int i=0; i<v; ++i ) {
+            if(!vis[i]) {
+                if(dfsChk(i, adj, vis, pathvis)) return true;
+            }
+        }
+        return false;
+    }
 public:
     bool canFinish(int nodes, vector<vector<int>> &arr)
     {
@@ -10,33 +37,6 @@ public:
         {
             adj[it[0]].push_back(it[1]);
         }
-        vector<int> indegree(nodes, 0);
-        for (int i = 0; i < nodes; ++i)
-        {
-            for (auto &it : adj[i])
-            {
-                indegree[it]++;
-            }
-        }
-        queue<int> q;
-        for (int i = 0; i < nodes; ++i)
-        {
-            if (!indegree[i])
-                q.push(i);
-        }
-        int cnt = 0;
-        while (!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            cnt++;
-            for (auto &it : adj[node])
-            {
-                indegree[it]--;
-                if (!indegree[it])
-                    q.push(it);
-            }
-        }
-        return cnt == nodes;
+        return !isCyclic(adj, nodes);
     }
 };
