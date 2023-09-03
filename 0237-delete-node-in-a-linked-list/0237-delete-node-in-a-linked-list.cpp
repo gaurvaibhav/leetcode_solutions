662eaead317cb322648b07ec8e463b8f49d80896
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-03 22:05:53 +0530

    Time: 10 ms (47.49%), Space: 7.8 MB (21.86%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -6,9 +6,11 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        FASTIO;
         *node = *node->next;
     }
 };
