e4eee691a115a2fcbf22d23a115bfc82f75688d0
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-09 08:33:20 +0530

    Time: 4 ms (63.14%), Space: 10.8 MB (37.67%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,23 +1,26 @@
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution {
 public:
-    void findC(int ind,int target,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds, int n){
+    void findC(int ind,int target,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds){
+        FASTIO;
         if(target < 0) return;
         if(target == 0){
             ans.push_back(ds);
             return;
         }
-        for(int i=ind;i<n;i++){
+        for(int i=ind;i<arr.size();i++){
             if(i>ind && arr[i]==arr[i-1]) continue;
             ds.push_back(arr[i]);
-            findC(i+1,target-arr[i],arr,ans,ds,n);
+            findC(i+1,target-arr[i],arr,ans,ds);
             ds.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        FASTIO;
         sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        findC(0,target,candidates,ans,ds,candidates.size());
+        findC(0,target,candidates,ans,ds);
         return ans;
     }
 };
