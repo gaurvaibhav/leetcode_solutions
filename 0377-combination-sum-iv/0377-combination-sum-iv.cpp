58a36855d0e66fc5a26d3450b7dcbec5bcfb5f75
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-09 10:07:32 +0530

    Time: 4 ms (28.06%), Space: 6.9 MB (12.44%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,22 +1,20 @@
 #define op ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define ll long long
 class Solution {
-private:
-    int findC(int ind,int target,vector<int> &arr, int n, vector<int>& dp){
-        op;
-        if(target < 0) return 0;
-        if(target == 0) return 1;
-        if(dp[target]!=-1) return dp[target];
-        int res = 0;
-        for(int i=0; i<n; ++i) {
-            res += findC(i, target-arr[i], arr, n, dp);
-        }
-        return dp[target] = res;
-    }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         op;
         int n = nums.size();
-        vector<int> dp(target+1, -1);
-        return findC(0, target, nums, n, dp);
+        ll MOD = 1e18+7;
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=target; ++i) {
+            for(int j=0; j<n; ++j) {
+                if(i >= nums[j]) {
+                    dp[i] = (1ll*dp[i] + dp[i-nums[j]]) % MOD;
+                }
+            }
+        }
+        return dp[target];
     }
 };
