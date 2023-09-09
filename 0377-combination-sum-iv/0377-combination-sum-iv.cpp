182bdec8bde0ab2cb14a0439334a75dc6c123c44
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-09 09:44:09 +0530

    Time: 4 ms (28.06%), Space: 6.7 MB (20.91%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,22 +1,22 @@
 #define op ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution {
 private:
-    int findC(int ind,int target,vector<int> &arr, int n, vector<vector<int>>& dp){
+    int findC(int ind,int target,vector<int> &arr, int n, vector<int>& dp){
         op;
         if(target < 0) return 0;
         if(target == 0) return 1;
-        if(dp[ind][target]!=-1) return dp[ind][target];
+        if(dp[target]!=-1) return dp[target];
         int res = 0;
         for(int i=0; i<n; ++i) {
             res += findC(i, target-arr[i], arr, n, dp);
         }
-        return dp[ind][target] = res;
+        return dp[target] = res;
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         op;
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (target+1, -1));
+        vector<int> dp(target+1, -1);
         return findC(0, target, nums, n, dp);
     }
 };
