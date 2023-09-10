fc4d9f77748570f8c0ab9bdc9bfa67fc9ca1311a
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-10 23:00:56 +0530

    Time: 0 ms (100%), Space: 6.6 MB (18.28%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,14 +1,17 @@
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 #define ll long long
 #define MOD 1000000007
 class Solution {
 public:
     ll solve(ll n, vector<ll>& dp) {
+        FASTIO;
         if (n == 0) return 1;
         if (dp[n] != -1) return dp[n];
         ll res = solve(n - 1, dp) * (2 * n - 1) * n % MOD;
         return dp[n] = res;
     }
     int countOrders(int n) {
+        FASTIO;
         vector<ll> dp(n+1, -1);
         return solve(n, dp);
     }
