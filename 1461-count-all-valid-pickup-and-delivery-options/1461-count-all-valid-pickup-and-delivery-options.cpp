48a7214589893f0fea9dcaecf3c021787473a12a
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-10 23:18:05 +0530

    Time: 0 ms (100%), Space: 6.5 MB (19.56%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -2,20 +2,17 @@
 #define ll long long
 #define MOD 1000000007
 class Solution {
-private:
-    ll solve(ll p, ll d, vector<vector<int>>& dp) {
-        FASTIO;
-        if(p == 0 && d == 0) return 1;
-        if(p<0 || d<0 || p>d) return 0;
-        if(dp[p][d] != -1) return dp[p][d];
-        ll p1 = p * solve(p-1, d, dp);
-        ll p2 = (d-p) * solve(p, d-1, dp);
-        return dp[p][d] = (p1+p2) % MOD;
-    }
 public:
+    ll solve(ll n, vector<ll>& dp) {
+        FASTIO;
+        if (n == 0) return 1;
+        if (dp[n] != -1) return dp[n];
+        ll res = solve(n - 1, dp) * (2 * n - 1) * n % MOD;
+        return dp[n] = res;
+    }
     int countOrders(int n) {
         FASTIO;
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        return solve(n, n, dp);
+        vector<ll> dp(n+1, -1);
+        return solve(n, dp);
     }
 };
