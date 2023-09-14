2b5d7b5460174bb7020ada65e927ffe7a88c0e14
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-14 13:15:22 +0530

    Time: 74 ms (33.83%), Space: 57 MB (19.94%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,17 +3,13 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         FASTIO;
-        int mx = nums[0], i = 1;
         unordered_map<int, int> mp;
         for(auto& it: nums) {
-            mx = max(mx, it);
-            if(it > 0 && !mp[it]) mp[it]++;
+            mp[it]++;
         }
-        for(auto& it: mp) {
+        for(int i=1; i<=nums.size()+1; ++i) {
             if(!mp[i]) return i;
-            i++;
         }
-        if(mp.empty()) return 1;
-        return ++mx;
+        return 1;
     }
 };
