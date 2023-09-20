a8b8dc4c08104a0bb8ce89408ef62293a94a8bce
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-09-20 12:01:42 +0530

    Time: 3 ms (88.7%), Space: 9.1 MB (5.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,9 +8,11 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        FASTIO;
         if(!head || !head->next) return head;
         ListNode *newHead = NULL, *headNext = NULL;
         newHead = reverseList(head->next);
