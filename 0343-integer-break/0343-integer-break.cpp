4a85983478ddb51d3c159459e6dbdf33a65333f4
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-10-06 09:30:44 +0530

    Time: 4 ms (12.57%), Space: 7.4 MB (6.83%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,22 +1,21 @@
 #define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
-#define ll long long
-#define MOD 1000000007
 class Solution
 {
 private:
-    int solve(int ind, int n, vector<vector<int>>& dp)
+    vector<vector<int>> dp;
+    int solve(int ind, int n)
     {
         FASTIO;
         if(ind == 0 || n == 0) return 1;
         if(dp[n][ind] != -1) return dp[n][ind];
-        if(ind > n) return dp[n][ind] = solve(ind-1, n, dp);
-        return dp[n][ind] = max((ind * solve(ind, n-ind, dp)), solve(ind-1, n, dp));
+        if(ind > n) return dp[n][ind] = solve(ind-1, n);
+        return dp[n][ind] = max((ind * solve(ind, n-ind)), solve(ind-1, n));
     } 
 public:
     int integerBreak(int n)
     {
         FASTIO;
-        vector<vector<int>> dp(n+1, vector<int> (n, -1));
-        return solve(n-1, n, dp);
+        dp = vector<vector<int>>(n+1, vector<int> (n, -1));
+        return solve(n-1, n);
     }   
 };
