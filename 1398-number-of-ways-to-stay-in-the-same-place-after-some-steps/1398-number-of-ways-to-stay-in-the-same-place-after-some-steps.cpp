15666f6ad3d08d5cb32f812c258f446cca5079ac
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-10-15 15:20:07 +0530

    Time: 22 ms (53.46%), Space: 14 MB (33.98%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,22 @@
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define MOD 1000000007
+class Solution
+{
+private:
+    vector<vector<int>> dp;
+    int solve(int ind, int k, int n)
+    {
+        FASTIO;
+        if(k == 0 && ind == 0) return 1;
+        if(ind < 0 || ind >= n || k == 0 || ind > k) return 0;
+        if(dp[ind][k] != -1) return dp[ind][k];
+        return dp[ind][k] = (((solve(ind+1, k-1, n)) % MOD + (solve(ind-1, k-1, n))) % MOD + (solve(ind, k-1, n)))% MOD;
+    }
+public:
+    int numWays(int k, int n)
+    {
+        FASTIO;
+        dp = vector<vector<int>> (k+1, vector<int> (k+1, -1));
+        return solve(0, k, n);
+    }
+};
