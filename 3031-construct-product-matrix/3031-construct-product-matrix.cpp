eff2bde8bcc14dae2d0e483a226beb5d541942c1
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-10-15 11:07:38 +0530

    Time: 225 ms (50%), Space: 163.6 MB (33.33%) - LeetHub

---
+++ 3031-construct-product-matrix/3031-construct-product-matrix.cpp
@@ -0,0 +1,47 @@
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+#define ll long long
+#define MOD 12345
+class Solution
+{
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid)
+    {
+        FASTIO;
+        ll p, ans;
+        int n, m, i, j;
+        n = grid.size();
+        m = grid[0].size();
+        vector<vector<ll>> pref(n, vector<ll> (m));
+        vector<vector<ll>> suff(n, vector<ll> (m));
+        vector<vector<int>> res(n, vector<int> (m));
+        p = 1;
+        for(i=n-1; i>=0; --i)
+        {
+            for(j=m-1; j>=0; --j)
+            {
+                suff[i][j] = p;
+                p *= grid[i][j];
+                p %= MOD;
+            }
+        }
+        p = 1;
+        for(i=0; i<n; ++i)
+        {
+            for(j=0; j<m; ++j)
+            {
+                pref[i][j] = p;
+                p *= grid[i][j];
+                p %= MOD;
+            }
+        }
+        for(i=0; i<n; ++i)
+        {
+            for(j=0; j<m; ++j)
+            {
+                ans = pref[i][j] * suff[i][j];
+                res[i][j] = ans % MOD;
+            }
+        }
+        return res;
+    }
+};
