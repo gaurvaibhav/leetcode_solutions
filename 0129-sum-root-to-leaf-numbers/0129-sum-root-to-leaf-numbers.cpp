e1b22bd837ffc30091fdeb4413d145314ef40353
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-10-16 22:29:39 +0530

    Time: 0 ms (100%), Space: 9.7 MB (13.47%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,28 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution
 {
-private:
-    int solve(TreeNode* root, int curr)
-    {
-        if(!root) return 0;
-        curr = curr * 10 + root->val;
-        if(!root->left && !root->right)
-        return curr;
-        return solve(root->left, curr) + solve(root->right, curr);
-    }
 public:
     int sumNumbers(TreeNode* root)
     {
-        return solve(root, 0);
+        FASTIO;
+        int res;
+        res = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while(q.size())
+        {
+            auto [n, curr] = q.front();
+            q.pop();
+            curr = curr * 10 + n->val;
+            if(n->left) q.push({n->left, curr});
+            if(n->right) q.push({n->right, curr});
+            if(!n->left && !n->right) res += curr;
+        }
+        return res;
     }
 };
