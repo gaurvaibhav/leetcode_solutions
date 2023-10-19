e8239f6cc2030d61d084dbc3e9e06cf2c59d2ee2
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-10-19 20:24:38 +0530

    Time: 0 ms (100%), Space: 7.5 MB (50.08%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -3,8 +3,6 @@ class Solution
 public:
     string gcdOfStrings(string s1, string s2)
     {
-        if(s1 + s2 == s2 + s1)
-        return s1.substr(0, gcd(s1.size(), s2.size()));
-        return "";
+        return (s1 + s2 == s2 + s1) ? s1.substr(0, gcd(s1.size(), s2.size())) : "";
     }
 };
