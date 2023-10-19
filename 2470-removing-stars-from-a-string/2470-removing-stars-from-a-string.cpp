b7f414275a1e54e5879cbd45c0b3e9c58a69180c
Vaibhav Gaur <77144204+gaurvaibhav@users.noreply.github.com>
2023-10-19 09:05:59 +0530

    Time: 48 ms (99.12%), Space: 25.7 MB (86.69%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -1,15 +1,28 @@
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 class Solution
 {
 public:
     string removeStars(string s)
     {
+        FASTIO;
         string res;
-        int i, n = s.length();
-        for(i=0; i<n; ++i)
+        int i, j, n = s.length();
+        i = j = 0;
+        for(j=0; j<n; ++j)
         {
-            if(s[i] == '*' && !res.empty()) res.pop_back();
-            else if(s[i] != '*') res.push_back(s[i]);
+            if(s[j] == '*')
+            {
+                --i;
+                i = max(0, i);
+            }
+            else 
+            {
+                s[i] = s[j];
+                i++;
+            }
         }
+        for(j=0; j<i; ++j)
+            res.push_back(s[j]);
         return res;
     }
 };
